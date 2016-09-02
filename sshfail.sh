bd59d9059b2c73262226751f8fafc8509cab1f5c
nevetsanderson <steveabrahall@gmail.com>
2016-09-03 00:58:12 +1000

    Updated V 0.03
    Improved ip extraction and date variables to deal with first of month date padding

--- sshfail.sh
+++ sshfail.sh
@@ -1,19 +1,21 @@
-#Change to log directory
 cd /var/log/
 #used for file names
 now=$(date +"%m_%d_%Y_%T")
 
 #used for todays date
-sdate=$(date +"%b %d")
+sdate=$(date +"%b %e")
 
-echo `date` >> ~/sshfail/report_$now
+echo `date` >> ~/aholes/aholefile_$now
 
-echo "Number of attempts to hack my machine" >> ~/sshfail/report_$now
-cat auth.log | grep "$sdate"| grep "refused connect from" | cut -d " " -f 9 | wc -l >> ~/sshfail/report_$now
+echo "Number of attempts to hack my machine" >> ~/aholes/aholefile_$now
+cat auth.log | grep "$sdate"| grep "refused connect from" | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"  | wc -l >> ~/aholes/aholefile_$now
+echo  "Number of unique IP address that junk came from" >> ~/aholes/aholefile_$now
+cat auth.log | grep "$sdate"| grep "refused connect from" | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | sort -u  | wc -l  >> ~/aholes/aholefile_$now
+echo >> ~/aholes/aholefile_$now
+echo "These are their ip address" >> ~/aholes/aholefile_$now
+echo " " >> ~/aholes/aholefile_$now
 
-echo  "Number of unique IP address that junk came from" >> ~/sshfail/report_$now
-cat auth.log | grep "$sdate"| grep "refused connect from" | cut -d " " -f 9 | sort -u | wc -l >> ~/sshfail/report_$now
-echo
-echo "These are their ip address" >> ~/sshfail/report_$now
-echo " "
-cat auth.log | grep "$sdate" | grep "refused connect from" | cut -d " " -f 9 | sort -u >> ~/sshfail/report_$now
+
+cat auth.log | grep "$sdate"| grep "refused connect from" | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | sort -u  >> ~/aholes/aholefile_$now
+
+
