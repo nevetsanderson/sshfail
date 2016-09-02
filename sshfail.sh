bb5306fd4b2ec9caf26076c1cae9c229bea253c6
nevetsanderson <steveabrahall@gmail.com>
2016-09-03 01:03:46 +1000

    Update sshfail.sh

--- sshfail.sh
+++ sshfail.sh
@@ -5,17 +5,15 @@ now=$(date +"%m_%d_%Y_%T")
 #used for todays date
 sdate=$(date +"%b %e")
 
-echo `date` >> ~/aholes/aholefile_$now
+echo `date` >> ~/sshfail/report_$now
 
-echo "Number of attempts to hack my machine" >> ~/aholes/aholefile_$now
-cat auth.log | grep "$sdate"| grep "refused connect from" | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"  | wc -l >> ~/aholes/aholefile_$now
-echo  "Number of unique IP address that junk came from" >> ~/aholes/aholefile_$now
-cat auth.log | grep "$sdate"| grep "refused connect from" | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | sort -u  | wc -l  >> ~/aholes/aholefile_$now
-echo >> ~/aholes/aholefile_$now
-echo "These are their ip address" >> ~/aholes/aholefile_$now
-echo " " >> ~/aholes/aholefile_$now
-
-
-cat auth.log | grep "$sdate"| grep "refused connect from" | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | sort -u  >> ~/aholes/aholefile_$now
+echo "Number of attempts to hack my machine" >> /sshfail/report_$now
+cat auth.log | grep "$sdate"| grep "refused connect from" | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"  | wc -l >> ~/sshfail/report_$now
+echo  "Number of unique IP address that junk came from" >> ~/sshfail/report_$now
+cat auth.log | grep "$sdate"| grep "refused connect from" | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | sort -u  | wc -l  >> ~/sshfail/report_$now
+echo >> ~/sshfail/report_$now
+echo "These are their ip address" >> ~/sshfail/report_$now
+echo " " >> ~/sshfail/report_$now
 
 
+cat auth.log | grep "$sdate"| grep "refused connect from" | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | sort -u  >> ~/sshfail/report_$now
