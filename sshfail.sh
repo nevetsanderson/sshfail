966309234792ac2809a59a99303cefc3a6611fbd
nevetsanderson <steveabrahall@gmail.com>
2016-09-03 12:19:20 +1000

    Update sshfail.sh

--- sshfail.sh
+++ sshfail.sh
@@ -4,16 +4,15 @@ now=$(date +"%m_%d_%Y_%T")
 
 #used for todays date
 sdate=$(date +"%b %e")
-
+#Date stamp the report
 echo `date` >> ~/sshfail/report_$now
-
-echo "Number of attempts to hack my machine" >> /sshfail/report_$now
+#generates report data
+echo "Number of attempts to hack my machine" >> ~/sshfail/report_$now
 cat auth.log | grep "$sdate"| grep "refused connect from" | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"  | wc -l >> ~/sshfail/report_$now
 echo  "Number of unique IP address that junk came from" >> ~/sshfail/report_$now
 cat auth.log | grep "$sdate"| grep "refused connect from" | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | sort -u  | wc -l  >> ~/sshfail/report_$now
 echo >> ~/sshfail/report_$now
 echo "These are their ip address" >> ~/sshfail/report_$now
 echo " " >> ~/sshfail/report_$now
-
 
 cat auth.log | grep "$sdate"| grep "refused connect from" | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | sort -u  >> ~/sshfail/report_$now
